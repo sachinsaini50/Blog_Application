f26a8637f6c0dd9703ab7c6ad69610c8a4ac35fb
sachin <sachin@gmail.com>
2025-09-06 18:33:55 +0530

    updated files

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,5 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sainisachin91738@gmail.com'
-EMAIL_HOST_PASSWORD = 'rwlgxmwcinjuwiwv'
 DEFAULT_FROM_EMAIL = 'sainisachin91738@gmail.com'
